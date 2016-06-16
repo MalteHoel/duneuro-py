a02522306c0c3682cda0b2012e6f3830a53afe06
Andreas Nüßing <andreas.nuessing@uni-muenster.de>
2016-06-16 11:20:10 +0200

    extend python wrapper
    * add eeg analytic solution
    * add points on sphere generation
    * return log as a python dict

--- src/duneuro-py.cc
+++ src/duneuro-py.cc
@@ -7,8 +7,11 @@
 #include <pybind11/stl.h>
 
 #include <dune/common/parametertree.hh>
+#include <dune/common/std/memory.hh>
 
 #include <duneuro/common/dense_matrix.hh>
+#include <duneuro/common/points_on_sphere.hh>
+#include <duneuro/eeg/eeg_analytic_solution.hh>
 #include <duneuro/io/dipole_reader.hh>
 #include <duneuro/io/field_vector_reader.hh>
 #include <duneuro/io/projections_reader.hh>
@@ -19,6 +22,10 @@ namespace py = pybind11;
 // translate a dune exception to std::exception since the latter is automatically converted to a
 // python error
 struct DubioException : public std::exception {
+  explicit DubioException(const std::string& msg) : what_(msg)
+  {
+  }
+
   explicit DubioException(const Dune::Exception& ex) : what_(ex.what())
   {
   }
@@ -29,6 +36,55 @@ struct DubioException : public std::exception {
   std::string what_;
 };
 
+struct PyDictStorage : public duneuro::StorageInterface {
+  virtual void store(const std::string& name, const std::string& value)
+  {
+    dict[py::str(name)] = py::str(value);
+  }
+
+  virtual void storeMatrix(const std::string& name,
+                           std::shared_ptr<duneuro::MatrixInterface<double>> matrix)
+  {
+  }
+
+  virtual void storeMatrix(const std::string& name,
+                           std::shared_ptr<duneuro::MatrixInterface<unsigned int>> matrix)
+  {
+  }
+
+  py::dict dict;
+};
+
+std::string py_to_string(const py::handle& handle)
+{
+  std::string type = py::object(handle.get_type().attr("__name__")).cast<std::string>();
+  if (type == "str")
+    return handle.cast<std::string>();
+  else if (type == "int")
+    return std::to_string(handle.cast<int>());
+  else if (type == "float") {
+    std::stringstream sstr;
+    sstr << handle.cast<double>();
+    return sstr.str();
+  } else if (type == "bool")
+    return std::to_string(handle.cast<bool>());
+  else if (type == "list") {
+    std::vector<std::string> entries;
+    std::transform(handle.begin(), handle.end(), std::back_inserter(entries), py_to_string);
+    if (entries.size() > 0) {
+      std::stringstream str;
+      str << entries[0];
+      for (unsigned int i = 1; i < entries.size(); ++i) {
+        str << " " << entries[i];
+      }
+      return str.str();
+    } else {
+      return "";
+    }
+  }
+  throw DubioException(std::string("type \"") + type + std::string("\" not supported"));
+}
+
 // translate a python dict to a Dune::ParameterTree. The dict can be nested and the sub dicts will
 // be translated recursively. Note that the type of the values is assumed to be str
 Dune::ParameterTree dictToParameterTree(const py::dict& dict)
@@ -36,11 +92,11 @@ Dune::ParameterTree dictToParameterTree(const py::dict& dict)
   Dune::ParameterTree tree;
   for (const auto& item : dict) {
     std::string type = py::object(item.second.get_type().attr("__name__")).cast<std::string>();
-    if (type == "str") {
-      tree[item.first.cast<std::string>()] = item.second.cast<std::string>();
-    } else if (type == "dict") {
+    if (type == "dict") {
       auto subtree = dictToParameterTree(py::dict(item.second, true));
-      tree.sub(item.first.cast<std::string>()) = subtree;
+      tree.sub(py_to_string(item.first)) = subtree;
+    } else {
+      tree[py_to_string(item.first)] = py_to_string(item.second);
     }
   }
   return tree;
@@ -52,15 +108,16 @@ void register_field_vector(py::module& m)
 {
   using FieldVector = Dune::FieldVector<T, dim>;
   py::class_<FieldVector>(m, (std::string("FieldVector") + std::to_string(dim) + "D").c_str())
+      .def_buffer([](FieldVector& m) -> py::buffer_info {
+        return py::buffer_info(
+            &m[0], /* Pointer to buffer */
+            sizeof(T), /* Size of one scalar */
+            py::format_descriptor<T>::value(), /* Python struct-style format descriptor */
+            1, /* Number of dimensions */
+            {dim}, /* Buffer dimensions */
+            {sizeof(T)});
+      })
       .def(py::init<T>())
-      .def("__init__",
-           [](FieldVector& instance, const py::tuple& data) {
-             FieldVector vec;
-             for (unsigned int i = 0; i < dim; ++i) {
-               vec[i] = py::object(data[i]).cast<T>();
-             }
-             new (&instance) FieldVector(vec);
-           })
       .def(py::self += py::self)
       .def(py::self -= py::self)
       .def(py::self += T())
@@ -85,16 +142,6 @@ void register_dipole(py::module& m)
   py::class_<Dipole>(m, "Dipole")
       .def(py::init<const FieldVector&, const FieldVector&>(),
            "create a dipole from its position and moment", py::arg("position"), py::arg("moment"))
-      .def("__init__",
-           [](Dipole& instance, const py::tuple& position, const py::tuple& moment) {
-             FieldVector pos, mom;
-             for (unsigned int i = 0; i < dim; ++i) {
-               pos[i] = py::object(position[i]).cast<T>();
-               mom[i] = py::object(moment[i]).cast<T>();
-             }
-             new (&instance) Dipole(pos, mom);
-           },
-           "create a dipole from its position and moment", py::arg("position"), py::arg("moment"))
       .def("position", [](const Dipole& dip) { return dip.position(); }, "position",
            py::return_value_policy::copy)
       .def("moment", [](const Dipole& dip) { return dip.moment(); }, "moment",
@@ -106,11 +153,38 @@ void register_dipole(py::module& m)
       });
 }
 
+template <class F>
+py::dict output_call(F func)
+{
+  auto storage = std::make_shared<PyDictStorage>();
+  auto res = func(duneuro::DataTree(storage));
+
+  py::dict result;
+  result[py::str("config")] = storage->dict;
+  result[py::str("result")] = py::cast(res.release());
+  return result;
+}
+
+template <class F>
+py::dict void_call(F func)
+{
+  auto storage = std::make_shared<PyDictStorage>();
+  func(duneuro::DataTree(storage));
+  py::dict result;
+  result[py::str("config")] = storage->dict;
+  return result;
+}
+
 template <class T, int dim>
 void register_read_dipoles(py::module& m)
 {
-  m.def("read_dipoles",
-        [](const std::string& filename) { return duneuro::DipoleReader<T, dim>::read(filename); });
+  m.def("read_dipoles", [](const std::string& filename) {
+    return output_call([&](duneuro::DataTree dtree) {
+      auto res = Dune::Std::make_unique<std::vector<duneuro::Dipole<T, dim>>>();
+      duneuro::DipoleReader<T, dim>::read(filename, std::back_inserter(*res), dtree);
+      return std::move(res);
+    });
+  });
 }
 
 template <class T, int dim>
@@ -118,7 +192,11 @@ void register_field_vector_reader(py::module& m)
 {
   auto name = "read_" + std::to_string(dim) + "d_field_vectors";
   m.def(name.c_str(), [](const std::string& filename) {
-    return duneuro::FieldVectorReader<T, dim>::read(filename);
+    return output_call([&](duneuro::DataTree dtree) {
+      auto res = Dune::Std::make_unique<std::vector<Dune::FieldVector<T, dim>>>();
+      duneuro::FieldVectorReader<T, dim>::read(filename, std::back_inserter(*res), dtree);
+      return std::move(res);
+    });
   });
 }
 
@@ -127,7 +205,11 @@ void register_projections_reader(py::module& m)
 {
   auto name = "read_" + std::to_string(dim) + "d_projections";
   m.def(name.c_str(), [](const std::string& filename) {
-    return duneuro::ProjectionsReader<T, dim>::read(filename);
+    return output_call([&](duneuro::DataTree dtree) {
+      auto res = Dune::Std::make_unique<std::vector<std::vector<Dune::FieldVector<T, dim>>>>();
+      duneuro::ProjectionsReader<T, dim>::read(filename, std::back_inserter(*res), dtree);
+      return std::move(res);
+    });
   });
 }
 
@@ -210,17 +292,27 @@ void register_meeg_driver_interface(py::module& m)
       .def("makeDomainFunction", &duneuro::MEEGDriverInterface::makeDomainFunction)
       .def("solveEEGForward",
            [](duneuro::MEEGDriverInterface& interface, const duneuro::Dipole<double, 3>& dipole,
-              duneuro::Function& solution) { interface.solveEEGForward(dipole, solution); })
+              duneuro::Function& solution) {
+             return void_call([&](duneuro::DataTree dtree) {
+               interface.solveEEGForward(dipole, solution, dtree);
+             });
+           })
       .def("solveEEGForward",
            [](duneuro::MEEGDriverInterface& interface, const duneuro::Dipole<double, 3>& dipole) {
-             auto solution = interface.makeDomainFunction();
-             interface.solveEEGForward(dipole, solution);
-             return solution;
+             return output_call([&](duneuro::DataTree dtree) {
+               auto solution =
+                   Dune::Std::make_unique<duneuro::Function>(interface.makeDomainFunction());
+               interface.solveEEGForward(dipole, *solution, dtree);
+               return std::move(solution);
+             });
            })
       .def("solveMEGForward",
            [](duneuro::MEEGDriverInterface& interface, const duneuro::Function& eegSolution) {
              try {
-               return interface.solveMEGForward(eegSolution);
+               return output_call([&](duneuro::DataTree dtree) {
+                 return Dune::Std::make_unique<std::vector<double>>(
+                     interface.solveMEGForward(eegSolution, dtree));
+               });
              } catch (Dune::Exception& ex) {
                throw DubioException(ex);
              }
@@ -246,30 +338,37 @@ void register_meeg_driver_interface(py::module& m)
       .def("evaluateAtElectrodes", &duneuro::MEEGDriverInterface::evaluateAtElectrodes)
       .def("computeEEGTransferMatrix",
            [](duneuro::MEEGDriverInterface& interface) {
-             return interface.computeEEGTransferMatrix().release();
+             return output_call([&](duneuro::DataTree dtree) {
+               return interface.computeEEGTransferMatrix(dtree);
+             });
            })
       .def("computeMEGTransferMatrix",
            [](duneuro::MEEGDriverInterface& interface) {
-             return interface.computeMEGTransferMatrix().release();
+             return output_call([&](duneuro::DataTree dtree) {
+               return interface.computeMEGTransferMatrix(dtree);
+             });
            })
       .def("applyTransfer", [](duneuro::MEEGDriverInterface& interface, py::buffer& buffer,
                                const duneuro::MEEGDriverInterface::DipoleType& dipole) {
-        /* Request a buffer descriptor from Python */
-        py::buffer_info info = buffer.request();
+        return output_call([&](duneuro::DataTree dtree) {
+          /* Request a buffer descriptor from Python */
+          py::buffer_info info = buffer.request();
 
-        /* Some sanity checks ... */
-        if (info.format != py::format_descriptor<double>::value())
-          throw std::runtime_error("Incompatible format: expected a double array!");
+          /* Some sanity checks ... */
+          if (info.format != py::format_descriptor<double>::value())
+            throw std::runtime_error("Incompatible format: expected a double array!");
 
-        if (info.ndim != 2)
-          throw std::runtime_error("Incompatible buffer dimension!");
+          if (info.ndim != 2)
+            throw std::runtime_error("Incompatible buffer dimension!");
 
-        if (info.strides[1] / sizeof(double) != 1)
-          throw std::runtime_error("Supporting only row major format");
+          if (info.strides[1] / sizeof(double) != 1)
+            throw std::runtime_error("Supporting only row major format");
 
-        duneuro::DenseMatrix<double> transferMatrix(info.shape[0], info.shape[1],
-                                                    static_cast<double*>(info.ptr));
-        return interface.applyTransfer(transferMatrix, dipole);
+          duneuro::DenseMatrix<double> transferMatrix(info.shape[0], info.shape[1],
+                                                      static_cast<double*>(info.ptr));
+          return Dune::Std::make_unique<std::vector<double>>(
+              interface.applyTransfer(transferMatrix, dipole, dtree));
+        });
       });
   ;
 }
@@ -302,6 +401,35 @@ void register_dense_matrix(py::module& m)
   });
 }
 
+void register_points_on_sphere(py::module& m)
+{
+  m.def("generate_points_on_sphere", [](const py::dict& d) {
+    auto ptree = dictToParameterTree(d);
+    try {
+      return duneuro::generate_points_on_sphere<double, 3>(ptree);
+    } catch (Dune::Exception& ex) {
+      throw DubioException(ex);
+    }
+  });
+}
+
+void register_analytical_solution(py::module& m)
+{
+  m.def("analytical_solution",
+        [](const std::vector<Dune::FieldVector<double, 3>>& electrodes,
+           const duneuro::Dipole<double, 3>& dipole, const py::dict& config) {
+          auto ptree = dictToParameterTree(config);
+          return output_call([&](duneuro::DataTree dtree) {
+            try {
+              return Dune::Std::make_unique<std::vector<double>>(
+                  duneuro::compute_analytic_solution(electrodes, dipole, ptree, dtree));
+            } catch (Dune::Exception& ex) {
+              throw DubioException(ex);
+            }
+          });
+        });
+}
+
 PYBIND11_PLUGIN(duneuropy)
 {
   py::module m("duneuropy", "duneuropy library");
@@ -320,6 +448,10 @@ PYBIND11_PLUGIN(duneuropy)
   register_meeg_driver_factory(m);
 
   register_dense_matrix<double>(m);
+
+  register_points_on_sphere(m);
+
+  register_analytical_solution(m);
 
   return m.ptr();
 }
